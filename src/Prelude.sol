f545fbbedf0180b557b87332f2e26f940c65750b
Joshua Trujillo <jtriley15@gmail.com>
2023-08-14 18:05:50 -0700

    new prelude

--- src/Prelude.sol
+++ src/Prelude.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-// Re-Re-Export
-//
-// have fun tracing this one kiddo
+import {
+    Primitive,
+    Constants as PrimitiveConstants,
+    As as PrimitiveAs,
+    Error as PrimitiveError
+} from "src/primitive/Primitive.sol";
 
-import "src/primitive/Mod.sol";
+import {
+    Option,
+    Constants as OptionConstants,
+    Error as OptionError,
+    LibOption
+} from "src/option/Option.sol";
 
-
+import {
+    SmartPointer,
+    Constants as SmartPointerConstants,
+    Error as SmartPointerError,
+    LibSmartPointer
+} from "src/smart-pointer/SmartPointer.sol";
